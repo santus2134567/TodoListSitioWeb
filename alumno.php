76cc740791678dbacc94a0012ffeac5b70fea912
ProgramacionMovil app01 <201913176e@utea.edu.pe>
2022-10-16 14:59:45 -0500

    Creacion de index.php y carpetas de recursos

---
+++ alumno.php
@@ -0,0 +1,22 @@
+<?php include("template/cabecera.php"); ?>
+
+<div class="jumbotron">
+        <a class="d-flex" href="#">
+            <img  src="" alt="">
+        </a>
+    </div>
+    <center>
+        <h1 class="display-3">UNIVERSIDAD TECNOLOGIA DE LOS ANDES </h1>
+    </center>
+    <p class="lead">ING. DE SISTEMAS E INFORMATICA</p>
+    <hr class="my-2">
+    <p>
+    <ul class="list-group">
+        <li class="list-group-item active">ALUMNO: MANUEL BRANDON GUZMAN MAMANI</li>
+        <li class="list-group-item">CODIGO: 201913176E </li>
+        <li class="list-group-item disabled">CURSO: INGENIERIA DE SOFTWARE </li>
+        <li class="list-group-item disabled">DOCENTE: EDUARDO CHAVEZ VASQUEZ </li>
+    </ul>
+</div>
+
+<?php include("template/pie.php"); ?>
--- index.php
+++ index.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    hola mundo
-</body>
-</html>
+<?php include ("template/cabecera.php"); ?>
+            <div class="jumbotron">
+                <h1 class="display-3">Jumbo heading</h1>
+                <p class="lead">Jumbo helper text</p>
+                <hr class="my-2">
+                <p>More info</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+                </p>
+            </div>
+        </div>
+    </div>
+<?php include ("template/pie.php"); ?>
